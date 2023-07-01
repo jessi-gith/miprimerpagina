e1314c2ca227779986fe992a6c6567232b8cb704
Jessica Ledesma <noeliledesma460@gmail.com>
2023-07-01 15:22:52 -0300

    styles
    styles

--- script.js
+++ script.js
@@ -29,5 +29,16 @@ form.addEventListener('submit',function(event){
 document.querySelector("button.button-menu-toggle")
 .addEventListener("click", function() {
     document.querySelector(".nav-links").classList.toggle("nav-links-responsive")
-})
+});
+
+const images =[
+    "/img/imagen1.jpg", 
+    "/img/imagen2.jpg",
+    "/img/imagen3.jpg",
+];
+let currentImageIndex = 0;
+
+let botonAnterior.
+
+
             
